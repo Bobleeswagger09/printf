daa4bdae69ceb846f1e41db6aae55b1a668da38b
Michelle Odigie <mitchyodigie1@gmail.com>
2023-08-22 16:51:25 +0100

    task 0 correction

--- handle_str.c
+++ handle_str.c
@@ -11,12 +11,28 @@
 
 void handle_string(const char *str, char *buffer, int *buff_ind, int *count)
 {
+	if (str == NULL)
+	{
+		handle_string("(null)", buffer, buff_ind, count);
+		return;
+	}
 	while (*str)
 	{
 		if (*buff_ind == BUFFER_SIZE - 1)
 		{
 			*count = _write_buffer(buffer, buff_ind, count);
 		}
+
+		if (*str < 32 || *str >= 127)
+		{
+			buffer[(*buff_ind)++] = '\\';
+			buffer[(*buff_ind)++] = 'x';
+			buffer[(*buff_ind)++] = "0123456789ABCDEF"[(*str >> 4) & 0x0F];
+			buffer[(*buff_ind)++] = "0123456789ABCDEF"[*str & 0x0F];
+			str++;
+			continue;
+		}
+
 		buffer[*buff_ind] = *str;
 		(*buff_ind)++;
 		str++;
